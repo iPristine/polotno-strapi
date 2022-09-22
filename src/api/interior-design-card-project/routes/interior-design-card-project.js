483379477391775b6f2a77f9e9f2cbb188850f0d
Daniel <wasdrfff@yandex.ru>
2022-09-22 17:10:00 +0500

    perf(components): add new collection and component

---
+++ src/api/interior-design-card-project/routes/interior-design-card-project.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * interior-design-card-project router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::interior-design-card-project.interior-design-card-project');
---
+++ src/api/interior-desing-slider/routes/interior-desing-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * interior-desing-slider router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::interior-desing-slider.interior-desing-slider');
