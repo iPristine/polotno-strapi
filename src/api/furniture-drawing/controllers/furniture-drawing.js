78b4005aae75c96e9ae0676094c4305b25effa47
Daniel <wasdrfff@yandex.ru>
2022-09-22 14:36:57 +0500

    fix(components):add new collection

---
+++ src/api/furniture-drawing/controllers/furniture-drawing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * furniture-drawing controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::furniture-drawing.furniture-drawing');
---
+++ src/api/furniture-slider/controllers/furniture-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * furniture-slider controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::furniture-slider.furniture-slider');
