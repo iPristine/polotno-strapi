8ecce4d95713d1b07a48fb45fba3affba8e49b25
Daniel <wasdrfff@yandex.ru>
2022-09-26 15:10:38 +0500

    perf(components):add new type material-page

---
+++ src/api/material-page/routes/material-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * material-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::material-page.material-page');
---
+++ src/api/material-page/services/material-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * material-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::material-page.material-page');
