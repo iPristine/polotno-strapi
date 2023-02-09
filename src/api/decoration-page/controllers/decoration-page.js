6d2e58b8f38bca803c4a1322510757baa154b3f7
Daniel <wasdrfff@yandex.ru>
2023-02-09 17:42:05 +0500

    add new collections

---
+++ src/api/decoration-page/controllers/decoration-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * decoration-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::decoration-page.decoration-page');
---
+++ src/api/decoration-page/routes/decoration-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * decoration-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::decoration-page.decoration-page');
